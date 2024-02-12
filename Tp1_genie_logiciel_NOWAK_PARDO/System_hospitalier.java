981a63dfe08144eadb70cb7d7db187c643581d94
PARDO Tom NOWAK Ugo <nowak.ugo31@gmail.com>
2024-02-12 17:23:52 +0100

    Version commentée

---
+++ Tp1_genie_logiciel_NOWAK_PARDO/System_hospitalier.java
@@ -0,0 +1,48 @@
+package tp_git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class System_hospitalier {
+
+    private List<Requête> listRequetes;
+
+    public System_hospitalier(List<Requête> listRequetes) {
+        super();
+        this.listRequetes = listRequetes;
+    }
+
+    public System_hospitalier() {
+        super();
+        listRequetes = new ArrayList<>();
+    }
+
+    public List<Requête> getListRequetes() {
+        return listRequetes;
+    }
+
+    public void setListRequetes(List<Requête> listRequetes) {
+        this.listRequetes = listRequetes;
+    }
+
+    public void recevoirRequete(Requête requete) {
+        listRequetes.add(requete);
+        String analyse =  analyserRequete(requete);
+        String comparaisonDonnees = comparerDonnes();
+        System.out.println(analyse);
+        System.out.println(comparaisonDonnees);
+    }
+
+    private String analyserRequete(Requête requête) {
+	
+        /*Ce programme permet d'analyser la requ�te du 	  	   patient*/
+        return "requête analysée";
+    }
+
+    private String comparerDonnes() {
+	   /*Ce programme permet de comparer les donn�es du 	  	   patient*/
+        
+        return "Cluster trouvé";
+    }
+
+}
